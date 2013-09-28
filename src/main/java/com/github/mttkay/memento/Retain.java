f76a48133eb6d732e3eb36a9439c7672f081d21a
Matthias Kaeppler <m.kaeppler@gmail.com>
2013-09-28 22:23:11 +0200

    Restrict @Retain to fields

--- src/main/java/com/github/mttkay/memento/Retain.java
+++ src/main/java/com/github/mttkay/memento/Retain.java
@@ -1,8 +1,11 @@
 package com.github.mttkay.memento;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
 public @interface Retain {
 }
