5d53a88a45b7ea1b4c230c097a41ece3bf546793
Matthias Kaeppler <m.kaeppler@gmail.com>
2013-10-13 17:11:27 +0200

    Separate processor from client library code

--- memento-processor/src/test/java/com/github/mttkay/memento/MementoProcessorTest.java
+++ memento-processor/src/test/java/com/github/mttkay/memento/MementoProcessorTest.java
@@ -4,13 +4,23 @@ import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
 
 import com.google.testing.compile.CompilationFailureException;
 import com.google.testing.compile.JavaFileObjects;
+import com.sun.tools.internal.xjc.util.NullStream;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.truth0.Truth;
 
+import java.io.PrintStream;
+
 @RunWith(JUnit4.class)
 public class MementoProcessorTest {
+
+    @Before
+    public void dontPrintExceptions() {
+        // get rid of the stack trace prints for expected exceptions
+        System.setErr(new PrintStream(new NullStream()));
+    }
 
     @Test
     public void generateMementoClass() {
